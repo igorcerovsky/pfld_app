49bfd47ce36ee07f05f5c0ac6d0b9fec9f0c3619
igi <igor.cerovsky@gmail.com>
2015-04-20 14:40:37 +0200

    refactoring

--- pfld_app.cpp
+++ pfld_app.cpp
@@ -4,6 +4,7 @@
 #include "stdafx.h"
 #include "facet.hpp"
 #include "pfld_compute.hpp"
+#include "pfld_test_io.h"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -13,33 +14,39 @@
 
 using namespace std;
 
-const char* file_facets = "facets.txt";
-const int max_facets_to_generate = 10000;
-const int max_facets_to_load = max_facets_to_generate/1-5;
-const int max_points = 10000;
 
+const char* file_facets = "pfld_facets.txt";
+const char* file_points = "pfld_points.txt";
+const char* file_results = "pfld_results.txt";
+const int max_facets_to_generate = 10000;
+const int max_facets_to_load = max_facets_to_generate/100;
+const int max_points_to_generate = 10000;
+const int max_points_to_load = max_points_to_generate;
 
 using point = pfld::point;
 using ptvec = pfld::ptvec;
 using facet_vec = pfld::facet_vec;
 using facet = pfld::Facet;
 
-void GetFacets(pfld::facet_vec& facets);
-void GetFieldPoints(ptvec& points, const int n);
+#define GEN_FACETS false
+#define GEN_POINTS false
+#define SAVE_RESULTS
+
+
 void Compute(void(*FieldFn)(pfld::facet_vec&, pfld::ptvec&, pfld::valvec&),
 	pfld::facet_vec& facets, pfld::ptvec& fldPoints, pfld::valvec& outFld,
 	std::string message);
 
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	facet_vec facets;
-	GetFacets(facets);
+	GetFacets(facets, file_facets, max_facets_to_load, GEN_FACETS);
 
-	std::cout << "\n" << "initialize field points..." << "\n";
 	ptvec fldPts;
-	GetFieldPoints(fldPts, max_points);
+	GetFieldPoints(fldPts, file_points, max_points_to_load, GEN_POINTS);
 	
-	pfld::valvec outFld(max_points, 0.0);
+	pfld::valvec outFld(fldPts.size(), 0.0);
 	void(*FieldFn)(pfld::facet_vec&, pfld::ptvec&, pfld::valvec&);
 	FieldFn = pfld::Field_Gz;
 	Compute(FieldFn, facets, fldPts, outFld, "computing facets with parallel approach...");
@@ -56,82 +63,11 @@ int _tmain(int argc, _TCHAR* argv[])
 	FieldFn = pfld::Field_Gz_;
 	Compute(FieldFn, facets, fldPts, outFld4, "computing facets parallel future approach...");
 
-	return 0;
-}
-
-
-void GetFieldPoints(ptvec& points, const int n)
-{
-	std::srand(100);
-	for (int i = 0; i < n; ++i)
-	{
-		points.emplace_back(point(rand() % 4000 - 2000, rand() % 40000 - 2000, rand() % 200));
-	}
-}
-
-void GetFacets(pfld::facet_vec& facets)
-{
-//#define CREATE_RANDOM_FACETS
-#ifdef CREATE_RANDOM_FACETS
-	std::cout << "creating random facets..." << "\n";
-	std::srand(1);
-	for (int i = 0; i < max_facets_to_generate; ++i)
-	{
-		ptvec v{ point(rand() % 1001, rand() % 1001, -(rand() % 1001)),
-			point(rand() % 1001, rand() % 1001, -rand() % 1001),
-			point(rand() % 1001, rand() % 1001, -rand() % 1001) };
-		facets.emplace_back(facet(v));
-		if ((i % 1000) == 0)
-			std::cout << "\r" << i << " of " << max_facets_to_generate;
-	}
-
-	ofstream file(file_facets, ios::out);
-	if (file.is_open())
-	{
-		int i = 0;
-		for (auto it = facets.begin(); it != facets.end(); ++it)
-		{
-			ptvec& fpts = it->Data();
-			file << "Facet " << i << " " << fpts.size() << "\n";
-			for (auto itp = fpts.begin(); itp != fpts.end(); ++itp)
-				file << itp->x << " " << itp->y << " " << itp->z << " ";
-			file << "\n";
-			i++;
-		}
-		file.close();
-	}
-#else
-	std::cout << "loading facets..." << "\n";
-	ifstream file(file_facets, std::ios_base::in);
-	if (file.is_open())
-	{
-		std::string stmp;
-		ptvec v(3);
-		int loadedFacets = 0;
-		while (std::getline(file, stmp))
-		{
-			std::istringstream buffer(stmp);
-			if (stmp.find("Facet") != string::npos)
-			{
-				// facet data
-			}
-			else
-			{
-				std::vector<double> vln{ std::istream_iterator<double>(buffer),
-					std::istream_iterator<double>() };
-				for (unsigned i = 0; i < vln.size() / 3; ++i)	{
-					v[i] = point(vln[i * 3 + 0], vln[i * 3 + 1], vln[i * 3 + 2]);
-				}
-				facets.emplace_back(facet(v));
-				loadedFacets++;
-				if (loadedFacets >= max_facets_to_load)
-					break;
-			}
-		}
-		file.close();
-	}
-	std::cout << "loaded " << facets.size() << " facets." << "\n";
+#ifdef SAVE_RESULTS
+	pfld::SaveResults(outFld, file_results);
 #endif
+
+	return 0;
 }
 
 void Compute(void(*FieldFn)(pfld::facet_vec&, pfld::ptvec&, pfld::valvec&),
